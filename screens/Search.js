870a97b2bbc822689bbc7b4edf03b1524c08133c
Alex Yatseyko <alex.yatseiko@gmail.com>
2020-09-23 08:23:48 +0300

    Days are added

--- screens/Search.js
+++ screens/Search.js
@@ -21,16 +21,32 @@ export const Search = ({ route, navigation }) => {
     const { error, request } = useHttp()
     const { data } = route.params;
     const [city, setCity] = useState('')
-    
-    const getWeatherFull = async () => {    
+    const [weatherData, setWeatherData] = useState([])
+
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    const getWeatherFull = async () => {
         // const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=7&appid=${WEATHER_API_KEY}`
-        const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city.city}&cnt=7&appid=${WEATHER_API_KEY}`
+        // const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city.city}&cnt=7&appid=${WEATHER_API_KEY}`
+        
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.lon}&units=metric&exclude=hourly,minutely,current,alerts&appid=${WEATHER_API_KEY}`
+        // const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?q=london&units=metric&APPID=value&cnt=7&appid=${WEATHER_API_KEY}`
+        
+        // const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=7&appid=${WEATHER_API_KEY}`
+        // const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&units=metric&exclude=hourly,minutely,current,alerts&cnt=42&appid=${WEATHER_API_KEY}`
+        
         console.log(weatherUrl)
         try {
             const req = await request(weatherUrl)
-            console.log(req)
-            // setCurrentCity(req.name)
-            // setCurrentTemp(req.main.temp)
+            setWeatherData(req.daily)
+            // console.log(weatherData[0].temp.day)
+
+            // console.log(new Date(req.list[0].dt * 1000).getDay())
+            // console.log(req.list[0].dt)
+            // console.log(new Date(req.list[30].dt * 1000).getDay())
+            // console.log(req.list[30].dt)
+            // const newData = []
+            // console.log(newData)
         } catch (e) {
             console.log(e)
         }
@@ -38,7 +54,6 @@ export const Search = ({ route, navigation }) => {
 
     useEffect(() => {
         data ? setCity(data) : null
-        console.log(city)
     }, [])
 
     useEffect(() => {
@@ -71,12 +86,25 @@ export const Search = ({ route, navigation }) => {
                     <Icon name="search" size={25} color={'white'} />
                 </TouchableOpacity>
             </View>
-            <View style={styles.dataWrapper}>
-                <View style={styles.oneDayBlock}>
-                    <Text style={styles.oneDayBlockText}>Monday</Text>
-                    <Icon name="thermometer-half" size={25} color={'white'} />
-                </View>
-            </View>
+            <ScrollView style={styles.dataWrapper}>
+                {weatherData ? weatherData.map(e => {
+                    const numberOfTheDay = (new Date(e.dt * 1000).getDay())
+
+                    return (
+                        <View style={styles.oneDayBlock} key={e.dt}>
+                            <Text style={styles.oneDayBlockText}>{
+                                days[numberOfTheDay]}
+                            </Text>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Icon name="thermometer-half" size={25} color={'white'} />
+                                <Text style={styles.oneDayBlockText}>{e.temp.day}</Text>
+                            </View>
+                        </View>)
+                    // }
+                })
+                    : null}
+
+            </ScrollView>
         </SafeAreaView>
     )
 }
@@ -125,9 +153,12 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         paddingVertical: 15,
         alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
     },
     oneDayBlockText: {
         color: '#fff',
         fontSize: 20,
+        marginLeft: 5,
     },
 })
